d641e18a41b3c78f23f4cf2574d4bd19bccdcdd1
mr-spacklefish <52872339+mr-spacklefish@users.noreply.github.com>
2020-05-20 14:48:02 -0500

    Add files via upload

---
+++ src/pages/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <Layout>
+    <h1>About us</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque
+      omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum
+      odit fugit assumenda rem dolores inventore iste reprehenderit maxime!
+      Iusto.
+    </p>
+    <g-image
+      class="image"
+      alt="mountain bike"
+      src="~/assets/UwfTrails.jpg"
+    ></g-image>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "About us",
+  },
+};
+</script>
